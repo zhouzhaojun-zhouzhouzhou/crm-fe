efa885d479f0fc14d2bab6e5480c263622568dbb
zhaojun <1048317016@qq.com>
2020-05-06 18:21:46 +0800

    菜单增加

--- src/pages/goods-manage/goods-classify/index.vue
+++ src/pages/goods-manage/goods-classify/index.vue
@@ -3,97 +3,104 @@
 </template>
 
 <script>
-var echarts = require('echarts');
+var echarts = require('echarts')
 export default {
-//   name: 'hello',
-  data () {
-    return {
-    //   msg: 'Welcome to Your Vue.js App'
-    }
-  },
-  mounted(){
-    this.drawLine();
-  },
-  methods: {
-    drawLine(){
-        // 基于准备好的dom，初始化echarts实例
-        let myChart = this.$echarts.init(document.getElementById('myChart'))
-        // 绘制图表
-        myChart.setOption({
-            title: {
-        text: '产品分类',
-        left: 'center'
-    },
-    legend: {
-        left: 'center',
-        top: 'bottom',
-        data: ['产品1', '产品2', '产品3', '产品4', '产品5', '产品6', '产品7', '产品8']
-    },
-    toolbox: {
-        //show: true,
-        feature: {
-            //mark: {show: true},
-            //dataView: {show: true, readOnly: false},
-            // magicType: {
-            //     //show: true,
-            //     type: ['pie', 'funnel']
-            // },
-            // restore: {show: true},
-            // saveAsImage: {show: true}
+    //   name: 'hello',
+    data() {
+        return {
+            //   msg: 'Welcome to Your Vue.js App'
         }
     },
-    series: [
-        {
-            name: '半径模式',
-            type: 'pie',
-            radius: [20, 110],
-            center: ['45%', '50%'],
-            roseType: 'radius',
-            label: {
-                show: false
-            },
-            // emphasis: {
-            //     // label: {
-            //     //     show: false
-            //     // }
-            // },
-            // data: [
-            //     {value: 10, name: '产品1'},
-            //     {value: 5, name: '产品2'},
-            //     {value: 15, name: '产品3'},
-            //     {value: 25, name: '产品4'},
-            //     {value: 20, name: '产品5'},
-            //     {value: 35, name: '产品6'},
-            //     {value: 30, name: '产品7'},
-            //     {value: 40, name: '产品8'}
-            // ]
-        },
-        {
-            name: '面积模式',
-            type: 'pie',
-            radius: [30, 110],
-            center: ['50%', '50%'],
-            roseType: 'area',
-            left: 'center',
-            data: [
-                {value: 10, name: '产品1'},
-                {value: 5, name: '产品2'},
-                {value: 15, name: '产品3'},
-                {value: 25, name: '产品4'},
-                {value: 20, name: '产品5'},
-                {value: 35, name: '产品6'},
-                {value: 30, name: '产品7'},
-                {value: 40, name: '产品8'}
-            ]
+    mounted() {
+        this.drawLine()
+    },
+    methods: {
+        drawLine() {
+            // 基于准备好的dom，初始化echarts实例
+            let myChart = this.$echarts.init(document.getElementById('myChart'))
+            // 绘制图表
+            myChart.setOption({
+                title: {
+                    text: '产品分类',
+                    left: 'center'
+                },
+                legend: {
+                    left: 'center',
+                    top: 'bottom',
+                    data: [
+                        '产品1',
+                        '产品2',
+                        '产品3',
+                        '产品4',
+                        '产品5',
+                        '产品6',
+                        '产品7',
+                        '产品8'
+                    ]
+                },
+                toolbox: {
+                    //show: true,
+                    feature: {
+                        //mark: {show: true},
+                        //dataView: {show: true, readOnly: false},
+                        // magicType: {
+                        //     //show: true,
+                        //     type: ['pie', 'funnel']
+                        // },
+                        // restore: {show: true},
+                        // saveAsImage: {show: true}
+                    }
+                },
+                series: [
+                    {
+                        name: '半径模式',
+                        type: 'pie',
+                        radius: [20, 110],
+                        center: ['45%', '50%'],
+                        roseType: 'radius',
+                        label: {
+                            show: false
+                        }
+                        // emphasis: {
+                        //     // label: {
+                        //     //     show: false
+                        //     // }
+                        // },
+                        // data: [
+                        //     {value: 10, name: '产品1'},
+                        //     {value: 5, name: '产品2'},
+                        //     {value: 15, name: '产品3'},
+                        //     {value: 25, name: '产品4'},
+                        //     {value: 20, name: '产品5'},
+                        //     {value: 35, name: '产品6'},
+                        //     {value: 30, name: '产品7'},
+                        //     {value: 40, name: '产品8'}
+                        // ]
+                    },
+                    {
+                        name: '面积模式',
+                        type: 'pie',
+                        radius: [30, 110],
+                        center: ['50%', '50%'],
+                        roseType: 'area',
+                        left: 'center',
+                        data: [
+                            { value: 10, name: '产品1' },
+                            { value: 5, name: '产品2' },
+                            { value: 15, name: '产品3' },
+                            { value: 25, name: '产品4' },
+                            { value: 20, name: '产品5' },
+                            { value: 35, name: '产品6' },
+                            { value: 30, name: '产品7' },
+                            { value: 40, name: '产品8' }
+                        ]
+                    }
+                ]
+            })
         }
-    ]
-
-        });
     }
-  }
 }
 </script>
 
 <style scoped >
-
 </style>
--- src/pages/permission/menu-manage/index.vue
+++ src/pages/permission/menu-manage/index.vue
@@ -1,22 +1,31 @@
 <template>
-    <div>
-444444
-    </div>
+    <!-- 为 ECharts 准备一个具备大小（宽高）的 DOM -->
+    <div id="main" style="width: 600px;height:400px;"></div>
 </template>
+<script type="text/javascript">
+import echarts from 'echarts'
+// 基于准备好的dom，初始化echarts实例
 
-<script>
 export default {
-    props: {
-
-    },
-    data() {
-        return {
-            aaa: 2
+    data() {},
+    mounted() {
+        var myChart = echarts.init(document.getElementById('main'))
+        let option = {
+            xAxis: {
+                type: 'category',
+                data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+            },
+            yAxis: {
+                type: 'value'
+            },
+            series: [
+                {
+                    data: [820, 932, 901, 934, 1290, 1330, 1320],
+                    type: 'line'
+                }
+            ]
         }
+        myChart.setOption(option)
     }
 }
 </script>
-
-<style scoped lang="less">
-
-</style>
